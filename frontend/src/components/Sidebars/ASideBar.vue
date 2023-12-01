118c7e386a7395a2714f7c36d01747d1f99386db
HelloWorld <sc21m2w@leeds.ac.uk>
2023-12-01 21:00:00 +0800

    Nav Sidebar update

--- frontend/src/components/Sidebars/ASideBar.vue
+++ frontend/src/components/Sidebars/ASideBar.vue
@@ -1,45 +1,47 @@
 <script setup lang="ts">
-import { Icon } from '@arco-design/web-vue'
-
-const IconFont = Icon.addFromIconFontCn({
-  src: 'https://at.alicdn.com/t/font_180975_ue66sq60vyd.js'
-})
+// import { Icon } from '@arco-design/web-vue'
+//
+// const IconFont = Icon.addFromIconFontCn({
+//   src: 'https://at.alicdn.com/t/font_180975_ue66sq60vyd.js'
+// })
 </script>
 
 <template>
-  <div class="menu-demo border">
-    <a-menu mode="vertical" :default-selected-keys="['1']" style="width: min-content">
-      <a-menu-item key="0" style="padding: 0; /*margin-right: 38px;*/ margin-right: 0" disabled>
-        <div
-          class="logo"
-          style="
-            /*width: min-content;*/
-            /*width: 80px;*/
-            margin: 10px 0 10px 10px;
-            border: thin solid red;
-            /*width: 100%;*/
-            color: white;
-            border-radius: 2px;
-            /*background-color: transparent !important;*/
-            /*background-color: var(--color-fill-3);*/
-            /*cursor: text;*/
-          "
-        ></div>
-      </a-menu-item>
+  <div id="a-side-bar">
+    <div class="logo">
+      <div><a class="logo-img"></a></div>
+    </div>
+    <a-menu mode="vertical" :default-selected-keys="['1']" class="nav-menu">
       <a-menu-item key="1">
-        <!--        <a-image src="/images/home.svg" width="24" height="24" :preview="false"></a-image>-->
-        <div class="nav-icon" style="background-position: 0 0"></div>
-        首页
+        <a class="nav-a">
+          <div class="nav-icon"></div>
+          <div class="nav-text"><span>首页</span></div>
+        </a>
       </a-menu-item>
       <a-menu-item key="2">
-        <div class="nav-icon" style="background-position: -72px 0"></div>
-        推荐
+        <a class="nav-a">
+          <div class="nav-icon"></div>
+          <div class="nav-text"><span>推荐</span></div>
+        </a>
       </a-menu-item>
-      <a-menu-item key="3">关注</a-menu-item>
-      <a-menu-item key="4">朋友</a-menu-item>
-      <a-menu-item key="5">我的</a-menu-item>
+      <a-menu-item key="3">
+        <a class="nav-a">
+          <div class="nav-icon"></div>
+          <div class="nav-text"><span>关注</span></div>
+        </a></a-menu-item
+      >
+      <a-menu-item key="4">
+        <a class="nav-a">
+          <div class="nav-icon"></div>
+          <div class="nav-text"><span>朋友</span></div>
+        </a></a-menu-item
+      >
+      <a-menu-item key="5">
+        <a class="nav-a">
+          <div class="nav-icon"></div>
+          <div class="nav-text"><span>我的</span></div>
+        </a></a-menu-item
+      >
     </a-menu>
   </div>
 </template>
-
-<style scoped lang="scss"></style>
